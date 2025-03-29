07e0b76455f4631d08260274b9c3a9445df36e95
Madelyn Gross <mgross10@hydra8.eecs.utk.edu>
2025-03-29 19:25:29 -0400

    okay so I am going to get margs but I have 40 finished but will return tonight to get the rest

--- mymalloc.c
+++ mymalloc.c
@@ -2,7 +2,6 @@
 #include <stdlib.h> 
 #include <unistd.h>
 
-
 typedef struct chunk {
     size_t size;
     struct chunk *next;
@@ -12,11 +11,12 @@ typedef struct chunk {
 Chunk *free_list_head = NULL;
 
 void *traverse_free_list(size_t size){
-    //printf("Traversing free list\n");
+    printf("Traversing free list\n");
     Chunk *current = free_list_head;
     while(current != NULL){
+        printf("current size: %u\n", current->size);
         if(current->size >= size){
-            //printf("current: %ul\n", current);
+            printf("current: %ul\n", current);
             return current;
         }else{
             current = current->next;
@@ -26,55 +26,35 @@ void *traverse_free_list(size_t size){
 }
 
 void insert_chunk(Chunk *newChunk){
-    //printf("Inserting chunk\n");
+    printf("Inserting chunk again\n");
     if(free_list_head == NULL){
         free_list_head = newChunk;
+        return;
     }else if(free_list_head > newChunk){
         newChunk->next = free_list_head;
         free_list_head->prev = newChunk;
         free_list_head = newChunk;
     }else{
+        printf("do we get here?\n");
         Chunk *current = free_list_head;
-        while(current->next != NULL && !(current < newChunk)){
+        while(current->next != NULL && current->next < newChunk){
             current = current->next;
         }
-        newChunk->next = current;
-        newChunk->prev = current->prev;
-        current->prev = newChunk;
-        if(newChunk->prev != NULL){
-            newChunk->prev->next = newChunk;
+        newChunk->next = current->next;
+        newChunk->prev = current;
+        if (current->next != NULL) {
+            current->next->prev = newChunk;
         }
+        current->next = newChunk;
     }
 }
 
-void *my_malloc(size_t bytesToAllocate){
-    //printf("Allocating memory\n");
-    int size = (bytesToAllocate + 7 + 8) & -8;
-
-    Chunk *chunk = traverse_free_list(size);
-    if(chunk == NULL){
-        chunk = (Chunk *)sbrk(size > 8192 ? size : 8192);
-        chunk->size = size > 8192 ? size : 8192;
-        chunk->next = NULL;
-        chunk->prev = NULL;
-        insert_chunk(chunk);
-    }
-    //printf("hello?\n");
-    //printf("chunk size: %u\n", chunk->size);
-    if(chunk->size - size <= 16){
-        return chunk;
-    }
-    chunk->size -= size;
-
-    Chunk *newChunk = (Chunk *)((char *)chunk + chunk->size);
-    newChunk->size = size;
-
-    //printf("malloced chunk address?: %u\n", ((char *)chunk + chunk->size + 8));
-    return ((char *)chunk + chunk->size + 8);
-}
 
 void delete_chunk(Chunk *chunkToDelete){
-    //printf("Deleting chunk\n");
+    printf("Deleting chunk\n");
+    if (free_list_head == chunkToDelete) {
+        free_list_head = chunkToDelete->next;
+    }
     if(chunkToDelete->prev != NULL){
         chunkToDelete->prev->next = chunkToDelete->next;
     }
@@ -83,18 +63,60 @@ void delete_chunk(Chunk *chunkToDelete){
     }
 }
 
+void *my_malloc(size_t bytesToAllocate){
+    printf("Allocating memory\n");
+    int size = (bytesToAllocate + 7 + 8) & -8;
+
+    Chunk *chunk = traverse_free_list(size);
+    if(chunk == NULL){
+        printf("No chunk found, allocating new chunk\n");
+        chunk = (Chunk *)sbrk(size > 8192 ? size : 8192);
+        chunk->size = size > 8192 ? size : 8192;
+        chunk->next = NULL;
+        chunk->prev = NULL;
+        insert_chunk(chunk);
+    }
+    printf("hello?\n");
+    printf("new chunk size 1: %u\n", size);
+    printf("chunk size: %u\n", chunk->size);
+
+    if(chunk->size - size <= 16){ 
+        printf("we are in here\n");
+        delete_chunk(chunk);
+        printf("chunk size 1: %u\n", chunk->size);
+        return ((char *)chunk + 8);
+    }
+
+    chunk->size -= size;  // Reduce the size of the current chunk
+    printf("new chunk size 1: %u\n", size);
+    printf("chunk size 2: %u\n", chunk->size);
+
+    Chunk *newChunk = (Chunk *)((char *)chunk + chunk->size);
+    newChunk->size = size;  // Assign size to the new chunk
+    newChunk->next = NULL;
+    newChunk->prev = NULL;
+    
+    // Correct the chunk pointer arithmetic here
+    printf("malloced chunk address?: %u\n", ((char *)chunk + chunk->size + 8));
+    return (char*)newChunk + 8;
+}
+
+
+
 void my_free(void *ptr){
-    //printf("Freeing memory\n");
-    ptr -= 8;
-    Chunk *chunk = (Chunk *)ptr;
+    printf("Freeing memory\n");
+    char *charPtr = (char *)ptr - 8;
+    Chunk *chunk = (Chunk *)charPtr;
+    chunk->next = NULL;
+    chunk->prev = NULL;
     insert_chunk(chunk);
 }
 
-void coelasce_free_list(){
-    //printf("Coalescing free list\n");
+void coalesce_free_list(){
+    printf("Coalescing free list\n");
     Chunk *current = free_list_head;
     while(current != NULL){
-        if(current + current->size == current->next){
+        if ((char *)current + current->size == (char *)current->next){
             current->size += current->next->size;
             delete_chunk(current->next);
         }
@@ -103,12 +125,12 @@ void coelasce_free_list(){
 }
 
 void *free_list_begin(){
-    //printf("Free list begin\n");
+    printf("Free list begin\n");
     return free_list_head;
 }
 
 void *free_list_next(void *node){
-    //printf("Free list next\n");
+    printf("Free list next\n");
     Chunk *chunk = (Chunk *)node;
     return chunk->next;
 }
